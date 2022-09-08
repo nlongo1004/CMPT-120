936e7864605279b7fb667c5b8a0b428c111ceb96
nlongo1004 <112398240+nlongo1004@users.noreply.github.com>
2022-09-08 08:53:30 -0400

    changed function name, added in function

--- Assignment 1/helloworld.py
+++ Assignment 1/helloworld.py
@@ -1,17 +1,16 @@
-#instructions: print out "Hello World!"
-#create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
-#create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
+# instructions: print out "Hello World!"
+# create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
+# create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
+
+#I needed to change the name of the function because there was a conflict with main because they both are under the assignment 1 file
+def hello_world():
+    print("Hello World!")
+    fav_color = "purple"
+    print("My favorite color is: " + fav_color)
+    birthday = 10042003
+    print("My birthday is: " + str(birthday))
 
 
 
-def main():
-    print("Hello world!")
 
-
-
-    
-    
-    
-    
-    
-main()
+hello_world()

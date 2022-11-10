b8f96e2dff037e68c36766d403f6d93d6c02d19e
nlongo1004 <112398240+nlongo1004@users.noreply.github.com>
2022-11-10 10:15:53 -0500

    good

--- Assignment 6/student.py
+++ Assignment 6/student.py
@@ -1,4 +1,4 @@
-#oo scary
+# oo scary
 '''
 Instructions:
 We're now experts at classes, right?
@@ -24,15 +24,41 @@ create a function because this college is a wacky one- every day they generate a
 (disclosure: obviously there's a very small chance of your generated number matching the student id number. I just want to see that you're generating and comparing properly)
 '''
 
-class student:
-    
-    
-    
-    
-    
-    
+import random
+
+
+class Student:
+    def __init__(self, name: str, student_id: int, year: str, major: str, gpa: float):
+        self.name = name
+        self.student_id = student_id
+        self.year = year
+        self.major = major
+        self.gpa = gpa
+
+    def is_honors(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
+
+    def free_lunch(self):
+        winning_id = random.randint(1, 1000)
+        if self.student_id == winning_id:
+            return "Winner! " + self.name + " gets a free lunch!"
+        else:
+            return "Loser!"
+
+
 def main():
-    #create three students and check if they get free lunch and if they qualify for honors
-    
-    
+    # create three students and check if they get free lunch and if they qualify for honors
+    student1 = Student("Nick", 1, "f", "Computer Science", 4.0)
+    print(student1.is_honors(), student1.free_lunch())
+
+    student2 = Student("Valentina", 4, "f", "Business", 4.0)
+    print(student2.is_honors(), student2.free_lunch())
+
+    student3 = Student("Blob", 999, "s", "Rock Studies", 0.1)
+    print(student3.is_honors(), student3.free_lunch())
+
+
 main()
